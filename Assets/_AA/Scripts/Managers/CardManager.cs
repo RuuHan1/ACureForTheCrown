893c7f798969e22ada059dfcbf23031d3f829a75
Abdülhamid Uysal <hamituysal25@gmail.com>
2026-03-01 17:40:14 +0300

    some changes

--- Assets/_AA/Scripts/Managers/CardManager.cs
+++ Assets/_AA/Scripts/Managers/CardManager.cs
@@ -3,25 +3,28 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-    [SerializeField] private List<CardSO> cardDatas;
+    [SerializeField] private List<CardSO> cardDatas; // Ana liste (bozulmaz)
     [SerializeField] private GameObject _cardPrefab;
     [SerializeField] private RectTransform _cardPanel;
+
+    private List<CardSO> _activeCards = new List<CardSO>(); // Tüketilen liste
+
     private void OnEnable()
     {
         GameEvents.CardSwiped += OnCardSwiped;
         GameEvents.GameStarted += OnGameStarted;
     }
 
-    private void OnGameStarted()
-    {
-        GenerateCard();
-    }
-
     private void OnDisable()
     {
         GameEvents.CardSwiped -= OnCardSwiped;
         GameEvents.GameStarted -= OnGameStarted;
+    }
 
+    private void OnGameStarted()
+    {
+        RefillDeck();
+        GenerateCard();
     }
 
     private void OnCardSwiped(SwipeDirection direction, CardSO sO)
@@ -31,16 +34,34 @@ public class CardManager : MonoBehaviour
 
     private void GenerateCard()
     {
+        // Havuz boşsa yeniden doldur
+        if (_activeCards.Count == 0)
+        {
+            RefillDeck();
+        }
+
         GameObject gameObj = Instantiate(_cardPrefab, _cardPanel);
 
         RectTransform rt = gameObj.GetComponent<RectTransform>();
-
-        rt.localPosition = Vector3.one;
+        rt.localPosition = Vector3.zero; // Düzeltildi
         rt.localRotation = Quaternion.identity;
         rt.localScale = Vector3.one;
 
         Card card = gameObj.GetComponent<Card>();
-        int dice = Random.Range(0, cardDatas.Count);
-        card.Setup(cardDatas[dice]);
+
+        // Havuzdan rastgele kart seç
+        int dice = Random.Range(0, _activeCards.Count);
+        CardSO selectedCard = _activeCards[dice];
+
+        // Seçilen kartı havuzdan çıkar
+        _activeCards.RemoveAt(dice);
+
+        card.Setup(selectedCard);
+    }
+
+    private void RefillDeck()
+    {
+        _activeCards.Clear();
+        _activeCards.AddRange(cardDatas);
     }
 }
